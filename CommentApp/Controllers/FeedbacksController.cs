cc324a543bd0e63a622f2dbba4cd4961041d2766
Mayank Saxena <mayanksaxena2318@gmail.com>
2022-02-13 12:56:59 +0530

    Add project files.

---
+++ CommentApp/Controllers/FeedbacksController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CommentApp.Data;
+using CommentApp.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CommentApp.Controllers
+{
+    [Authorize]
+    public class FeedbacksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FeedbacksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Feedbacks
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Feedback.ToListAsync());
+        }
+
+        
+
+        // GET: Feedbacks/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Feedbacks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        
+        public async Task<IActionResult> Create([Bind("CommentId,User,Comment")] Feedback feedback)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(feedback);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Create));
+            }
+            return View(feedback);
+        }
+
+       
+
+        
+        
+    }
+}
---
+++ CommentApp/Data/Migrations/20220211123045_first.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CommentApp.Data.Migrations
+{
+    public partial class first : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Feedback",
+                columns: table => new
+                {
+                    CommentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    User = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Comment = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Feedback", x => x.CommentId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Feedback");
+        }
+    }
+}
